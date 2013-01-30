7d9b27eca3ac177a04b1fbfb167b2fbc16d3eab6
Yang Ruan <chuyi2007@hotmail.com>
2013-01-29 20:07:47 -0500

    LinkedLists

--- MergeTwoSortedList.java
+++ MergeTwoSortedList.java
@@ -50,3 +50,38 @@ public class Solution {
         return head;
     }
 }
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ListNode sen = new ListNode(0);
+        ListNode head = sen;
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                sen.next = l1;
+                l1 = l1.next;
+                sen = sen.next;
+            }
+            else{
+                sen.next = l2;
+                l2 = l2.next;
+                sen = sen.next;
+            }
+        }
+        sen.next = l1 == null?l2:l1;
+        return head.next;
+    }
+}
--- PartitionLists.java
+++ PartitionLists.java
@@ -48,3 +48,39 @@ public class Solution {
         return larger;
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ListNode small = new ListNode(0), big = new ListNode(0);
+        ListNode sen = small, pivot = big;
+        while(head != null){
+            if(head.val < x){
+                small.next = head;
+                small = small.next;
+                head = head.next;
+                small.next = null;
+            }
+            else{
+                big.next = head;
+                big = big.next;
+                head = head.next;
+                big.next = null;
+            }
+        }
+        small.next = pivot.next;
+        return sen.next;
+    }
+}
