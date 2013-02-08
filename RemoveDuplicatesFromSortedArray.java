65d75204cb3487687f84428a41c961c1754c8bef
Yang Ruan <yangruan@indiana.edu>
2013-02-08 16:42:51 -0500

    Clean Code

--- RemoveDuplicatesFromSortedArray.java
+++ RemoveDuplicatesFromSortedArray.java
@@ -2,33 +2,12 @@ public class Solution {
     public int removeDuplicates(int[] A) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        //O(N^2) solution
-        /*
-        if(A.length == 0 || A.length == 1)
-            return A.length;
-        int size = A.length;
-        for(int i = 0, j = 1; j < size;){
-            if(A[i] == A[j]){
-                for(int k = j; k < size; ++k)
-                    A[k - 1] = A[k];
-                --size;
-            }
-            else{
+        int uni = -1;
+        for(int i = 0; i < A.length; ++i){
+            while(i < A.length - 1 && A[i] == A[i + 1])
                 ++i;
-                ++j;
-            }
+            A[++uni] = A[i];
         }
-        return size;
-        */
-        int j = 0, n = A.length;
-        if(n < 1)
-	    return 0;
-        for(int i = 0; i < n;){
-            while(i < n && A[i] == A[j])
-                ++i;
-            if(i < n)
-                A[++j] = A[i];
-        }
-        return j + 1;
+        return uni + 1;
     }
 }
