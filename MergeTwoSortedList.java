4b95c7c991d3ee4402efbc95d0370f25fcd43b81
Yang Ruan <yangruan@indiana.edu>
2014-01-26 16:11:12 -0500

    Clean Code II

--- MergeTwoSortedList.java
+++ MergeTwoSortedList.java
@@ -9,58 +9,25 @@
  *     }
  * }
  */
-public class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode head = null, cur = null;
-        while (l1 != null && l2 != null) {
-            ListNode smaller = null;
-            if (l1.val < l2.val) {
-                smaller = l1;
-                l1 = l1.next;
-            }
-            else {
-                smaller = l2;
-                l2 = l2.next;
-            }
-            if (head == null) {
-                head = smaller;
-                cur = smaller;
-            }
-            else {
-                cur.next = smaller;
-                cur = cur.next;
-            }
-        }
-        if (head != null) {
-            cur.next = l1 == null ? l2 : l1;
-        }
-        else {
-            head = l1 == null ? l2 : l1;
-        }
-        return head;
-    }
-}
-
 //Use a sentry
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         // Start typing your Java solution below
         // DO NOT write main() function
         ListNode sen = new ListNode(0);
-        ListNode head = sen;
-        while(l1 != null && l2 != null){
-            if(l1.val < l2.val){
-                sen.next = l1;
+        ListNode pre = sen;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                pre.next = l1;
                 l1 = l1.next;
-                sen = sen.next;
             }
-            else{
-                sen.next = l2;
+            else {
+                pre.next = l2;
                 l2 = l2.next;
-                sen = sen.next;
             }
+            pre = pre.next;
         }
-        sen.next = l1 == null ? l2 : l1;
-        return head.next;
+        pre.next = l1 == null ? l2 : l1;
+        return sen.next;
     }
 }
