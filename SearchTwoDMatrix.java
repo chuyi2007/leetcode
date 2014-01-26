42c51142ccdb97842cd68479f2bd3f80a106033c
Yang Ruan <yangruan@indiana.edu>
2014-01-26 15:43:00 -0500

    Clean Code II

--- SearchTwoDMatrix.java
+++ SearchTwoDMatrix.java
@@ -30,12 +30,18 @@ public class Solution {
         // DO NOT write main() function
         int m = matrix.length, n = matrix[0].length;
         int i = 0, j = n - 1;
-        while (i < m && j >= 0) {
+        //remove overhead computing
+        boolean flag1 = true, flag2 = true;
+        while (i < m && j >= 0 && flag2) {
             if (matrix[i][j] > target) {
                 --j;
+                flag1 = false;
             }
             else if (matrix[i][j] < target) {
                 ++i;
+                if (!flag1) {
+                    flag2 = false;
+                }
             }
             else {
                 return true;
