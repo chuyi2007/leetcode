2fd0f045fa3a915a1a820b53b49b0e28621357cf
Yang Ruan <Ryan@Ryan-Macbook-Pro.local>
2015-11-10 23:20:08 -0800

    correct answer

--- CourseScheduleII.java
+++ CourseScheduleII.java
@@ -1,46 +1,41 @@
 public class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        Set<Integer>[] dependents = new Set[numCourses];
-        for (int i = 0; i < numCourses; ++i) {
-            dependents[i] = new HashSet<>();
-        }
-        for (int i = 0; i < prerequisites.length; ++i) {
-            int[] edge = prerequisites[i];
-            dependents[edge[1]].add(edge[0]);
-        }
-        int[] numPrere = new int[numCourses];
-        for (int i = 0; i < numCourses; ++i) {
-            for (int j: dependents[i]) {
-                ++numPrere[j];
+        Map<Integer, List<Integer>> dependents = new HashMap<>();
+        int[] degree = new int[numCourses];
+        for (int[] pre : prerequisites) {
+            if (!dependents.containsKey(pre[1])) {
+                dependents.put(pre[1], new ArrayList<>());
             }
+            dependents.get(pre[1]).add(pre[0]);
+            degree[pre[0]]++;
         }
-        
-        List<Integer> order = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
+        List<Integer> order = new ArrayList<>();
         for (int i = 0; i < numCourses; ++i) {
-            if (numPrere[i] == 0) {
+            if (degree[i] == 0) {
                 q.offer(i);
+                order.add(i);
             }
         }
-        
         while (!q.isEmpty()) {
-            int course = q.poll();
-            order.add(course);
-            for (int i: dependents[course]) {
-                if (--numPrere[i] == 0) {
-                    q.offer(i);
+            int cur = q.poll();
+            if (dependents.containsKey(cur)) {
+                for (int dep : dependents.get(cur)) {
+                    degree[dep]--;
+                    if (degree[dep] == 0) {
+                        q.offer(dep);
+                        order.add(dep);
+                    }
                 }
             }
         }
-        
-        return order.size() == numCourses ? convertToArray(order) : new int[0];
-    }
-    
-    public int[] convertToArray(List<Integer> order) {
-        int[] array = new int[order.size()];
-        for (int i = 0; i < order.size(); ++i) {
-            array[i] = order.get(i);
+        if (order.size() != numCourses) {
+            return new int[0];
         }
-        return array;
+        int[] res = new int[order.size()];
+        for (int i = 0; i < res.length; ++i) {
+            res[i] = order.get(i);
+        }
+        return res;
     }
 }
