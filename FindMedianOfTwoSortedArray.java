9c25875fd23c357948720ab3f6d84f21d145ce2f
Yang Ruan <Ryan@ryan-macbook-pro.attlocal.net>
2015-11-26 18:20:19 -0800

    recap uber

--- FindMedianOfTwoSortedArray.java
+++ FindMedianOfTwoSortedArray.java
@@ -1,23 +1,29 @@
- double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    int N1 = nums1.size();
-    int N2 = nums2.size();
-    if (N1 < N2) return findMedianSortedArrays(nums2, nums1);   // Make sure A2 is the shorter one.
-
-    if (N2 == 0) return ((double)nums1[(N1-1)/2] + (double)nums1[N1/2])/2;  // If A2 is empty
-
-    int lo = 0, hi = N2 * 2;
-    while (lo <= hi) {
-        int mid2 = (lo + hi) / 2;   // Try Cut 2 
-        int mid1 = N1 + N2 - mid2;  // Calculate Cut 1 accordingly
-
-        double L1 = (mid1 == 0) ? INT_MIN : nums1[(mid1-1)/2];  // Get L1, R1, L2, R2 respectively
-        double L2 = (mid2 == 0) ? INT_MIN : nums2[(mid2-1)/2];
-        double R1 = (mid1 == N1 * 2) ? INT_MAX : nums1[(mid1)/2];
-        double R2 = (mid2 == N2 * 2) ? INT_MAX : nums2[(mid2)/2];
-
-        if (L1 > R2) lo = mid2 + 1;     // A1's lower half is too big; need to move C1 left (C2 right)
-        else if (L2 > R1) hi = mid2 - 1;    // A2's lower half too big; need to move C2 left.
-        else return (max(L1,L2) + min(R1, R2)) / 2; // Otherwise, that's the right cut.
+public class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length, n2 = nums2.length;
+        if (n1 < n2) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        if (n2 == 0) {
+            return (nums1[n1 / 2] + nums1[(n1 - 1) / 2]) / 2.0;
+        }
+        // from 1 to n2 * 2 + 1
+        int left = 0, right = n2 * 2;
+        while (left <= right) {
+            int mid2 = (left + right) / 2;
+            int mid1 = n1 + n2 - mid2;
+            int l1 = mid1 == 0 ? Integer.MIN_VALUE : nums1[(mid1 - 1) / 2];
+            int l2 = mid2 == 0 ? Integer.MIN_VALUE : nums2[(mid2 - 1) / 2];
+            int r1 = mid1 == n1 * 2 ? Integer.MAX_VALUE : nums1[mid1 / 2];
+            int r2 = mid2 == n2 * 2 ? Integer.MAX_VALUE : nums2[mid2 / 2];
+            if (l1 > r2) {
+                left = mid2 + 1;
+            } else if (l2 > r1) {
+                right = mid2 - 1;
+            } else {
+                return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+            }
+        }
+        return -1;
     }
-    return -1;
-} 
+}
